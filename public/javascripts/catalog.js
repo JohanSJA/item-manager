20b4eee4cb265b0fe90a39c68eaf6b144872ee4c
Johan Sim Jian An <johansja@gmail.com>
2015-01-04 03:07:29 +0800

    Perform validation on client side.

--- public/javascripts/catalog.js
+++ public/javascripts/catalog.js
@@ -1,12 +1,35 @@
+function Pricing(data) {
+  var self = this;
+
+  self.cost = ko.observable(data.cost);
+  self.price = ko.observable(data.price);
+  self.promoPrice = data.promo_price;
+  self.savings = data.savings;
+  self.onSale = data.on_sale;
+
+  this.minPrice = ko.computed(function() {
+    return self.cost() + 0.01
+  });
+}
+
+function Item(data) {
+  var self = this;
+
+  self.id = data.id;
+  self.title = ko.observable(data.title);
+  self.pricing = new Pricing(data.pricing);
+}
+
 function CatalogViewModel() {
   var self = this;
   self.itemId = ko.observable("");
-  self.matchingProducts = ko.observableArray();
+  self.matchingItems = ko.observableArray();
 
   self.searchMatching = function(formElement) {
     var itemId = formElement.itemId.value;
     $.getJSON("/catalog/items?id=" + itemId, function(data) {
-      self.matchingProducts(data);
+      var mappedItems = $.map(data, function(item) { return new Item(item); });
+      self.matchingItems(mappedItems);
     });
   };
 
@@ -17,9 +40,9 @@ function CatalogViewModel() {
       "pricing": {
         "cost": Number(formElement.cost.value),
         "price": Number(formElement.price.value),
-        "promo_price": Number(formElement.promo_price.value),
+        "promo_price": Number(formElement.promoPrice.value),
         "savings": Number(formElement.savings.value),
-        "on_sale": Number(formElement.on_sale.value)
+        "on_sale": Number(formElement.onSale.value)
       }
     };
     $.ajax({
