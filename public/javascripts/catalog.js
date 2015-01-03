2dd8070fad2354d6e7cb3f357c064a1806d512a0
Johan Sim Jian An <johansja@gmail.com>
2015-01-04 01:57:45 +0800

    Perform update without validation so far.

--- public/javascripts/catalog.js
+++ public/javascripts/catalog.js
@@ -9,6 +9,26 @@ function CatalogViewModel() {
       self.matchingProducts(data);
     });
   };
+
+  self.updateItem = function(formElement) {
+    var data = {
+      "id": Number(formElement.id.value),
+      "title": formElement.title.value,
+      "pricing": {
+        "cost": Number(formElement.cost.value),
+        "price": Number(formElement.price.value),
+        "promo_price": Number(formElement.promo_price.value),
+        "savings": Number(formElement.savings.value),
+        "on_sale": Number(formElement.on_sale.value)
+      }
+    };
+    $.ajax({
+      url: "/catalog/items",
+      type: "PUT",
+      data: JSON.stringify(data),
+      contentType: "application/json"
+    });
+  }
 }
 
 ko.applyBindings(new CatalogViewModel());
